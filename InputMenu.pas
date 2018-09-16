9b1cd5ca83ab9f81310093ce8878761e62c157f5
irfannnnn <irfan.prakoso.idp@gmail.com>
2018-09-16 18:10:39 +0700

    input menu by 1301160164

---
+++ InputMenu.pas
@@ -0,0 +1,22 @@
+Program RegisterOwnerKaryawan;
+uses crt;
+
+var
+nama : string;
+harga : integer;
+
+begin
+writeln('==================');
+writeln('= INPUT MENU =');
+writeln('==================');
+
+write('Nama Menu : ');
+readln(nama);
+write('Harga Menu : ');
+readln(harga);
+
+if(nama<>null) and (harga<>null) then
+    writeln('Input Berhasil')
+else
+    writeln('Nama atau Harga tidak boleh kosong');
+end.
